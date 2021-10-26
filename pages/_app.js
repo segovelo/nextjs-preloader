e3fa58be28aab21c795cec8e7bc9ec1bb2071981
segovelo <gauchosinpingo@gmail.com>
2021-10-26 22:52:57 +0100

    Addition to screenloader.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
+import React from "react"
+import LoadingScreen from "../components/loadingscreen";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 5000);
+  }, []);
+
+  return (
+    <>
+      {!loading ? (
+        <React.Fragment>
+          <Component {...pageProps} />
+        </React.Fragment>
+      ) : (
+        <LoadingScreen />
+      )}
+    </>
+  );
 }
 
 export default MyApp
